211abd7b9b7357ed0065bf1d19c88d88ca1faccc
Callum Asplen <casplen@outlook.com>
2020-11-29 17:28:23 +0000

    MVP

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,106 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <h1>Pokedex</h1>
+    </header>
+    <div class="search">
+      <p>&#9654</p>
+      <input type="text" v-model="searchTerm" placeholder="Search...">
+    </div>
+    <div id="pokedex">
+      <pokemon-details v-for="pokemon in filteredPokemon" :key="pokemon.id" :pokemon="pokemon"></pokemon-details>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PokemonDetails from './components/PokemonDetails.vue'
 
 export default {
-  name: 'App',
+  name: "app",
+  data(){
+    return {
+      pokemonList: [],
+      searchTerm: ""
+    }
+  },
+  mounted(){
+    this.getPokemon()
+    this.sortPokemon()
+
+  },
+
+  computed:{
+    filteredPokemon(){
+            return this.pokemonList.filter((pokemon) => pokemon.name.includes(this.searchTerm.toLowerCase()))
+        }
+  },
+
+  methods: {
+    getPokemon: function() {
+      fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then(res => res.json())
+      .then(data => data.results.forEach(pokemon => this.getPokemonData(pokemon))
+      )
+    },
+    getPokemonData: function(pokemon) {
+      fetch(pokemon.url)
+      .then(res => res.json())
+      .then(data => this.pokemonList.push(data))
+    },
+    sortPokemon: function() {
+      let sortedPokemon = this.pokemonList.sort(function(a, b){
+        return a[id] < b[id] ? -1 : 1
+      })
+      return sortedPokemon = this.pokemonList
+    }
+  },
   components: {
-    HelloWorld
+    "pokemon-details": PokemonDetails
   }
+
 }
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Press Start 2P', cursive;
+}
+
+#pokedex {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin: 10px;
+  justify-content: center;
+}
+
+header {
+  background-color: #f73333;
+  padding: 10px;
+  color: white;
+  border-bottom: 5px solid black;
+}
+
+.search {
+  display: flex;
+  background-color: white;
+  padding: 3px;
+  border-bottom: 2px solid rgb(199, 199, 199);
+  padding-left: 5%
+}
+
+.search > input {
+  border: none;
+  padding-left: 15px;
+  margin-left: 15px;
+  width: 400px;
+  font-size: large;
+  font-family: helvetica;
+  font-weight: bold;
+}
+h1{
+  padding: 20px;
+  margin: 0px;
 }
 </style>
---
+++ src/components/PokemonDetails.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="pokedex-card">
+      <header>
+        <h3>
+          #{{pokemon.id}}: {{pokemon.name[0].toUpperCase() + pokemon.name.substring(1)}}
+        </h3>
+      </header>
+      <hr>
+      <div class="sprite-container">
+        <img :src="pokemon.sprites.front_default" alt="">
+      </div>
+      <hr>
+      <div class="type-container">
+        <p v-for="type in pokemon.types" :key=type.slot :class="typeClass(type.type.name)">{{type.type.name[0].toUpperCase() + type.type.name.substring(1)}}</p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'pokemon-details',
+    props: [ 'pokemon' ],
+    methods: {
+      typeClass(type) {
+        let className = "default";
+        let typeList = ["normal", "water", "electric", "fighting", "ground", "psychic", "rock", "dark", "steel", "fire", "grass", "ice", "poison", "flying", "bug", "ghost", "dragon", "fairy"]
+        typeList.forEach(typeClass => {
+          if (typeClass === type) {
+            className = typeClass
+          }
+        })
+        return className
+      }
+    }
+
+}
+</script>
+
+<style scoped>
+
+hr {
+  border: 1px solid rgb(199, 199, 199);
+  width: 90%;
+}
+
+h3 {
+  margin: 10px;
+  font-size: 1em;
+}
+
+.pokedex-card{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10px;
+  padding: 10px;
+  border: 1px solid rgb(199, 199, 199);
+  width: 300px;
+  background-color: white;
+}
+
+.type-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.type-container > p {
+  margin: 5px;
+  padding: 5px;
+  border: 1px solid grey;
+  color: white;
+  font-size: 0.8em;
+}
+
+.normal {
+  background-color: #A8A77A;
+}
+.water {
+  background-color: #6390F0;
+}
+.electric {
+  background-color: #F7D02C;
+}
+.fighting {
+  background-color: #C22E28;
+}
+.ground {
+  background-color: #E2BF65;
+}
+.psychic {
+  background-color: #F95587;
+}
+.rock {
+  background-color: #B6A136;
+}
+.dark {
+  background-color: #705746;
+}
+.steel {
+  background-color: #B7B7CE;
+}
+.fire {
+  background-color: #EE8130;
+}
+.grass {
+  background-color: #7AC74C;
+}
+.ice {
+  background-color: #96D9D6;
+}
+.poison {
+  background-color: #A33EA1;
+}
+.flying {
+  background-color: #A98FF3;
+}
+.bug {
+  background-color: #A6B91A;
+}
+.ghost {
+  background-color: #735797;
+}
+.dragon {
+  background-color: #6F35FC;
+}
+.fairy {
+  background-color: #D685AD;
+}
+
+</style>
